c73dfe71be219e85a7bb62d850bf42dce95486f5
Caley Woods <caley.w@gmail.com>
2018-08-02 16:27:53 -0500

    Add latest VS Code settings

--- vs_code_settings.js
+++ vs_code_settings.js
@@ -5,9 +5,9 @@
 
     // Misc visual tweaks
     "editor.lineHeight": 22,
-    
+
     // Font Settings
-    "editor.fontFamily": "Fira Code",
+    "editor.fontFamily": "Dank Mono",
     "editor.fontSize": 15,
     "editor.fontLigatures": true,
 
@@ -16,6 +16,8 @@
     "editor.insertSpaces": true,
     "editor.detectIndentation": false,
     "editor.renderIndentGuides": true,
+
+    "editor.hover.delay": 0,
 
     // Settings for Trailing Spaces Plugin
     "trailing-spaces.highlightCurrentLine": false,
@@ -62,6 +64,9 @@
     // No drag and drop for you
     "editor.dragAndDrop": false,
 
+    // Relative line numbering
+    "editor.lineNumbers": "relative",
+
     // WINDOW SETTINGS
 
     // On re-open restore all instances of VS Code that had folders open
@@ -87,15 +92,14 @@
     // Path to the git executable
     "git.path": "C:\\Program Files\\Git\bin\\git.exe",
 
-    // MISC
-
-    "vim.disableAnnoyingNeovimMessage": true,
-
     // WORKBENCH SETTINGS
 
     // Neither of these are installed by default
-    "workbench.colorTheme": "Cobalt2",
+    "workbench.colorTheme": "One Monokai",
     "workbench.iconTheme": "vscode-icons",
+    "workbench.colorCustomizations": {
+        "editorIndentGuide.activeBackground": "#ff0000"
+    },
 
     // Don't open welcome page or blank page when VS Code opens
     "workbench.startupEditor": "none",
@@ -103,4 +107,61 @@
     // Turn off the activity bar
     "workbench.activityBar.visible": false,
     "window.menuBarVisibility": "default",
+
+    "vsicons.dontShowNewVersionMessage": true,
+    "extensions.ignoreRecommendations": false,
+    "vsicons.projectDetection.autoReload": true,
+    "terminal.integrated.shell.windows": "C:\\Program Files\\Git\\bin\\bash.exe",
+
+    // Extension Settings
+
+    // VSCodeVim stuff
+
+    // Incremental search
+    "vim.incsearch": true,
+    // Highlight search results
+    "vim.hlsearch": true,
+    "vim.searchHighlightColor": "rgba(255, 81, 223, 0.3)",
+
+    // Hack to fix vimcode autoexpanding folded lines
+    "vim.foldfix": false,
+
+    // Let vsCodeVim use ctrl keys
+    "vim.useCtrlKeys": true,
+
+    // Turn off smart case which overrides case insensitivity if
+    // your search pattern contains uppercase chars
+    "vim.smartcase": false,
+
+    // Prefix commands with : in the command pallette
+    "vim.leader": "<space>",
+
+    // bind jj to <Esc> in insert mode
+    "vim.insertModeKeyBindings": [
+        {
+            "before": ["j", "j"],
+            "after": ["<Esc>"]
+        }
+    ],
+    // Turn on vim-easymotion
+    "vim.easymotion": true,
+
+    // Neovim integration
+    "vim.enableNeovim": false,
+    "vim.neovimPath": "C:\\Program Files\\Neovim\\bin\\nvim",
+
+    "vim.handleKeys": {
+        "<C-d>": false
+    },
+
+    // Twig Language 2
+    "files.associations": {
+        "*.html": "twig"
+    },
+    "emmet.includeLanguages": {
+        "twig": "html"
+    },
+
+    // Bracketpair colorizer
+    "bracketPairColorizer.forceIterationColorCycle": true,
 }
